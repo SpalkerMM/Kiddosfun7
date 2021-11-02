8d38e2c23050010f6296900c88f1bbf93fab504a
Shella <anandashella59@gmail.com>
2021-11-02 20:30:27 +0700

    Game Kucing

--- app/src/main/java/com/example/kiddosfun7/GamekucingActivity2.java
+++ app/src/main/java/com/example/kiddosfun7/GamekucingActivity2.java
@@ -13,6 +13,7 @@ public class GamekucingActivity2 extends AppCompatActivity {
         startActivity(intent);
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
